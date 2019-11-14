60365d83f6439dc16ba57dae6b88fa472171ef98
Thomas Kercheval <spacekattpoispin@gmail.com>
2019-11-14 15:47:30 -0800

    Add adjMatrixBuilder tests

--- tests/adjMatrixBuilder.test.ts
+++ tests/adjMatrixBuilder.test.ts
@@ -3,12 +3,20 @@ import {
 } from '../src/graph/adjMatrixBuilder';
 
 import {
+    Edge,
+} from '../src';
+import {
     Token,
     TokenOpts,
     TokenFactory,
     WORD,
     WordToken
 } from '../src/token';
+
+import {
+    isZeroMatrix,
+    numberArrayToEdges,
+} from './resources/shared';
 
 const tokenFactory = TokenFactory.createTokenFactory();
 
@@ -64,6 +72,80 @@ describe('Reee', () => {
         expect(adjMatrix.getNode(numberOfNodes - 1).kind).toEqual(WORD);
         expect(() => {
             adjMatrix.getNode(numberOfNodes).kind
+        }).toThrow(Error);
+    });
+
+    it('Adj Matrix is initialized to zero', () => {
+        const numberOfNodes = 5;
+        const builder = AdjacencyMatrixBuilder.newBuilder();
+
+        builder.withNodes(tokenGenerator(numberOfNodes));
+
+        const adjMatrix = builder.build();
+
+        expect(isZeroMatrix(adjMatrix)).toBe(true);
+    });
+
+    it('Is set with custom edges', () => {
+        const mat: number[][] = [
+            [1, 2],
+            [9, 4],
+        ];
+
+        const builder = AdjacencyMatrixBuilder.newBuilder();
+
+        builder.withNodes(tokenGenerator(mat.length));
+        builder.withEdges(numberArrayToEdges(mat));
+
+        const adjMatrix = builder.build();
+
+        expect(adjMatrix.getEdge(0, 0).score).toEqual(1);
+        expect(adjMatrix.getEdge(0, 1).score).toEqual(2);
+        expect(adjMatrix.getEdge(1, 0).score).toEqual(9);
+        expect(adjMatrix.getEdge(1, 1).score).toEqual(4);
+    });
+
+    it('Is clonable', () => {
+        const mat: number[][] = [
+            [7, 2],
+            [8, 3],
+        ];
+
+        const builder = AdjacencyMatrixBuilder.newBuilder();
+
+        builder.withNodes(tokenGenerator(mat.length));
+        builder.withEdges(numberArrayToEdges(mat));
+
+        const adjMatrix = builder.build();
+
+        const clonedBoi = AdjacencyMatrixBuilder.clone(adjMatrix);
+
+        expect(adjMatrix.getEdge(0, 0).score).toEqual(7);
+        expect(adjMatrix.getEdge(0, 1).score).toEqual(2);
+        expect(adjMatrix.getEdge(1, 0).score).toEqual(8);
+        expect(adjMatrix.getEdge(1, 1).score).toEqual(3);
+    });
+
+    it('Fails when given invalid edges', () => {
+        const builder = AdjacencyMatrixBuilder.newBuilder();
+
+        const edges: Edge[][] = [];
+
+        expect(() => {
+            builder.withEdges(edges)
+        }).toThrow(Error);
+    });
+
+    it('Fails when given no initialized nodes', () => {
+        const builder = AdjacencyMatrixBuilder.newBuilder();
+
+        const edges: Edge[][] = numberArrayToEdges([
+            [1, 2],
+            [2, 1],
+        ]);
+
+        expect(() => {
+            builder.withEdges(edges)
         }).toThrow(Error);
     });
 });
--- tests/resources/shared.ts
+++ tests/resources/shared.ts
@@ -1,7 +1,31 @@
 import {
+    AdjacencyMatrix,
     Edge,
 } from '../../src';
 
-const isZeroMatrix = (edges: Edge[][]): boolean => {
+export const isZeroMatrix = (adjMatrix: AdjacencyMatrix): boolean => {
+    const length = adjMatrix.getNumberNodes();
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length; j++) {
+            if (adjMatrix.getEdge(i, j).score !== 0) {
+                return false;
+            }
+        }
+    }
+
     return true;
+}
+
+export const numberArrayToEdges = (mat: number[][]): Edge[][] => {
+    const length =  mat.length;
+    const edges: Edge[][] = [];
+
+    for (let i = 0; i < length; i++) {
+        edges.push([]);
+        for (let j = 0; j < length; j++) {
+            edges[i].push({ score: mat[i][j] });
+        }
+    }
+    
+    return edges;
 }
