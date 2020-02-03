e12436e25199ae14750db9be956a60ae593e6e54
Thomas Kercheval <spacekattpoispin@gmail.com>
2020-02-02 22:34:30 -0800

    Add tests to expose algorithm bugs

--- src/tests/dijkstraSingle.test.ts
+++ src/tests/dijkstraSingle.test.ts
@@ -57,6 +57,54 @@ describe('DijkstraSinglePath', () => {
         expect(isEqualPaths(bestPath, knownBestPath)).toBeTruthy();
     });
 
+    it('Picks best path among options', () => {
+        const adjMatrix = AdjacencyMatrixBuilder
+            .newBuilder()
+            .withNodes(tokenGenerator(4))
+            .withEdges(Edge.getMatrixFromScoreMatrix([
+                [0, 1, 3, 0],
+                [0, 0, 1, 1],
+                [0, 0, 0, 2],
+                [0, 0, 0, 0]
+            ]))
+            .build();
+
+        const knownBestPath: Path = [0, 2, 3];
+
+        const pathStrategy = DijkstraSinglePath.create();
+
+        const pathIterator = pathStrategy.findPaths(adjMatrix);
+
+        const bestPath = pathIterator.next().value;
+
+        expect(isEqualPaths(bestPath, knownBestPath)).toBeTruthy();
+    });
+
+    it('Picks best path among more options', () => {
+        const adjMatrix = AdjacencyMatrixBuilder
+            .newBuilder()
+            .withNodes(tokenGenerator(6))
+            .withEdges(Edge.getMatrixFromScoreMatrix([
+                [ 0,  1, 33, 98,  0,  0],
+                [ 0,  0,  0,  3,  0,  1],
+                [ 0,  0,  0,  0, 17,  0],
+                [ 0,  0,  0,  0,  0,  1],
+                [ 0,  0,  0,  0,  0,  1],
+                [ 0,  0,  0,  0,  0,  0]
+            ]))
+            .build();
+
+        const knownBestPath: Path = [0, 3, 5];
+
+        const pathStrategy = DijkstraSinglePath.create();
+
+        const pathIterator = pathStrategy.findPaths(adjMatrix);
+
+        const bestPath = pathIterator.next().value;
+
+        expect(isEqualPaths(bestPath, knownBestPath)).toBeTruthy();
+    });
+
     it('Only picks one path', () => {
         const adjMatrix = AdjacencyMatrixBuilder
             .newBuilder()
